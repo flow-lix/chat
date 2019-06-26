12e7b90ca491baf396392dc01d5ddc0e16e89e86
flow-lix <2623970870@qq.com>
2019-06-26 17:08:17 +0800

    first blood

---
+++ src/main/java/org/lix/chat/LixChatApplication.java
@@ -0,0 +1,13 @@
+package org.lix.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LixChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LixChatApplication.class, args);
+    }
+
+}
